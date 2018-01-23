c9e0e9c1371c6c3d3a12276f02d6668bcabf947d
Jacob Arndt <jakewarndt@gmail.com>
2018-01-22 22:07:47 -0600

    small changes

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
   var navbartop = $('#portfolioNavbar').offset().top
-  
+  var windowHeight = $(window).height();
+
   $(window).scroll(function () {
 
     if ($(window).scrollTop() > navbartop) {
@@ -10,5 +11,12 @@ $(document).ready(function() {
     if ($(window).scrollTop() < navbartop+1) {
       $('#portfolioNavbar').removeClass('navbar-fixed');
     }
+    if ($(window).scrollTop() > windowHeight) {
+    	$('#portfolioNavbar').css({"background-color": "#52504f", "box-shadow": "1px 1px 1px #545251"}) 
+    }
+    if ($(window).scrollTop() < windowHeight) {
+    	$('#portfolioNavbar').css({"background-color": "transparent", "box-shadow": "0px 0px 0px transparent"}) 
+    }
   });
+
 });
