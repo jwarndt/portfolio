3087fb6b560fabca65d05c5a3de51f27811a9ea1
Jacob Arndt <arndt204@umn.edu>
2018-10-06 16:17:11 -0400

    updates

--- javascript/portfolio.js
+++ javascript/portfolio.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
   var navbartop = $('#portfolioNavbar').offset().top
-  var windowHeight = $(window).height();
+  var windowHeight = $(window).height() - 70;
+  var scrollSpeed = 1000;
 
   $(window).scroll(function () {
 
@@ -12,11 +13,32 @@ $(document).ready(function() {
       $('#portfolioNavbar').removeClass('navbar-fixed');
     }
     if ($(window).scrollTop() > windowHeight) {
-    	$('#portfolioNavbar').css({"background-color": "#52504f", "box-shadow": "1px 1px 1px #545251"}) 
+      $('#portfolioNavbar').css({"background-color": "#52504f", "box-shadow": "1px 1px 1px #545251"}) 
     }
     if ($(window).scrollTop() < windowHeight) {
-    	$('#portfolioNavbar').css({"background-color": "transparent", "box-shadow": "0px 0px 0px transparent"}) 
+      $('#portfolioNavbar').css({"background-color": "transparent", "box-shadow": "0px 0px 0px transparent"}) 
     }
+  });
+
+  $("#homeButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#background").offset().top}, scrollSpeed);
+  });
+  $("#aboutButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#aboutContainer").offset().top - 55}, scrollSpeed);
+  });
+  $("#projectsAndResearchButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#projectsAndResearchContainer").offset().top -55}, scrollSpeed);
+  });
+  $("#photoGalleryButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#photoGalleryContainer").offset().top-55}, scrollSpeed);
+  });
+  $("#contactButton").click(function() {
+    $('html,body').animate({
+      scrollTop: $("#contactContainer").offset().top}, scrollSpeed);
   });
 
 });
